3dff45b98683afd76f4ed2209aae00fd4c7d151c
MathisBD <mathis.bouverot@ens.psl.eu>
2024-09-20 15:22:00 +0200

    finished DeriveMapUniform

--- DeriveMapUniform.v
+++ DeriveMapUniform.v
@@ -1,12 +1,11 @@
-(**************************************************************************)
-(* Same as DeriveMap but more powerful. Still only one parameter though. *)
-(**************************************************************************)
+(* Derive a mapping function for inductive types which have exactly one *uniform* parameter. *)
 
 From elpi Require Import elpi.
 
+(**************************************************************************)
 (* We store all the mapping functions found so far in a database,
-   which we can extend at will (for instance when deriving a mapping function for
-   an inductive, we can add it to the database). *)
+   which we can extend at will. *)
+(**************************************************************************)
 Elpi Db map.db lp:{{
 
 % [lift-mapping A B FAB T U FTU] lifts the elementary mapping function [FAB : A -> B]
@@ -18,35 +17,24 @@ pred lift-mapping i:term, i:term, i:term, i:term, i:term, o:term.
 lift-mapping A B FAB A B FAB :- !.
 
 % Base case : the type T does not contain A.
-lift-mapping A B FAB T U {{ fun t : lp:T => t }} :-not (occurs A T), !.
+lift-mapping A B FAB T U {{ fun t : lp:T => t }} :- not (occurs A T), !.
 
-% More rules will be added below by coq.elpi.accumulate.
-
-%lift-mapping A B FAB {{ list lp:T }} {{ list lp:U }} {{ @List.map lp:T lp:U lp:FTU }} :- 
-%  lift-mapping A B FAB T U FTU.
-
-% ----------------------------------------------------------
-
-% Helper function to add a clause to lift-mapping. 
+% Helper function to generate a clause for lift-mapping. 
 % It takes as input a term [M] and its type of the form [forall (A B : Type), (A -> B) -> T -> U] 
 % where T does not contain B, and U is equal to T with A replaced by B. 
-% It returns a lift-mapping clause that tells how to lift a function A -> B to a function T -> U. 
+% It returns a lift-mapping clause that tells how to lift a function A -> B to a function T -> U.
 % This clause can then e.g. be added to the database using coq.elpi.accumulate.
 pred mk-lift-mapping i:term, i:term, o:prop.
 mk-lift-mapping M 
-  ({{ forall (A B : Type), (A -> B) -> lp:(T A B) -> lp:(U A B) }} as MTy)
-  (pi a b fab x y fxy\ lift-mapping a b fab (T x y) (U x y) {{ lp:M lp:x lp:y lp:fxy }} :- lift-mapping a b fab x y fxy) 
+  ({{ forall (A B : Type), (A -> B) -> lp:(T A) -> lp:(T B) }} as MTy)
+  (pi a b fab x y fxy\ lift-mapping a b fab (T x) (T y) {{ lp:M lp:x lp:y lp:fxy }} :- lift-mapping a b fab x y fxy) 
 :-
-  std.assert-ok! (coq.typecheck M MTy) "Invalid type.",
-  pi a b\
-  % Check T does not contain b.
-  not (occurs b (T a b)),
-  % Check U is equal to T with a replaced by b.
-  (copy a b => copy (T a b) (U a b)).
+  std.assert-ok! (coq.typecheck M MTy) "Invalid type.".
 }}. 
 
-
+(**************************************************************************)
 (* Command to extend the map database. *)
+(**************************************************************************)
 Elpi Command AddMap.
 Elpi Accumulate Db map.db.
 Elpi Accumulate lp:{{
@@ -57,6 +45,9 @@ main [trm M] :-
 }}.
 Elpi Typecheck.
 
+(**************************************************************************)
+(* Command to derive a mapping function for an inductive. *)
+(**************************************************************************)
 Elpi Command DeriveMap.
 Elpi Accumulate Db map.db.
 Elpi Accumulate lp:{{
@@ -65,12 +56,11 @@ Elpi Accumulate lp:{{
 main [str IndName] :- std.do! [
   % Locate the inductive type.
   std.assert! (coq.locate IndName (indt Ind)) "Could not find inductive type.",
-  % Check the inductive has exactly one parameter (uniform or not).
+  % Check the inductive has exactly one uniform parameter.
   (coq.env.indt-decl Ind (parameter _ _ ParamTy _\ inductive _ tt _ _)
-  ; coq.env.indt-decl Ind (inductive _ tt (parameter _ _ ParamTy _\ arity _) _)
-  ; coq.error "This command only supports inductive types with exactly one parameter."),
+  ; coq.error "This command only supports inductive types with exactly one uniform parameter."),
   % Check the parameter is a type.
-  std.assert! (ParamTy = sort _) "The inductive parameter's type should be a sort.",
+  std.assert! (ParamTy = sort _) "The inductive parameter's type should be Type.",
   % Build the map function.
   build-map Ind ParamTy F,
   coq.say { coq.term->string F },
@@ -81,7 +71,6 @@ main [str IndName] :- std.do! [
   coq.env.add-const FName F _ @transparent! C,
   coq.say "Added function under name" C,
   % Set implicit arguments for the function.
-  % For some reason we can't do this while building the function.
   coq.arguments.set-implicit (const C) [[ maximal, maximal, explicit, explicit ]]
 ].
 
@@ -92,61 +81,49 @@ pred build-map
   i:term,       % The type of the inductive parameter.
   o:term.       % The map function we are building.
 
-% We build a recursive function (a fixpoint).
+% We build a recursive function (a fixpoint). For technical reasons it is important that 
+% the actual [fix] occurs *after* binding the other arguments [A], [B] and [f].
 build-map I PTy 
-  {{ fix map (A B : lp:PTy) (f : A -> B) (x : lp:GI A) {struct x} : lp:GI B := lp:(Match map A B f x) }} 
+  {{ fun (A B : lp:PTy) (f : A -> B) => fix map (x : lp:(FI A)) {struct x} : lp:(FI B) := lp:(Match map A B f x) }} 
 :- std.do! [
-  GI = global (indt I),
-  MapTy = {{ forall (A B : lp:PTy), (A -> B) -> lp:GI A -> lp:GI B }},
+  (pi x\ coq.mk-app (global (indt I)) [x] (FI x)),
+  % Bind the first arguments.
+  @pi-decl `a` PTy a\
+  @pi-decl `b` PTy b\
+  @pi-decl `f` {{ lp:a -> lp:b }} f\
   % The fixpoint part. We use the argument [map] to recursively map over subterms of [x] 
   % which are of type [I A]. Technically this amounts to adding a (local) clause to lift-mapping.
-  @pi-decl `map` MapTy map\ mk-lift-mapping map MapTy (Rule map), Rule map =>
-  % Bind the remaining arguments.
-  @pi-decl `a` PTy a\ 
-  @pi-decl `b` PTy b\ 
-  @pi-decl `f` {{ lp:a -> lp:b }} f\
-  @pi-decl `x` {{ lp:GI lp:a }} x\
+  @pi-decl `map` (prod _ (FI a) _\ (FI b)) map\ 
+  % If [map] were polymorphic we could use mk-lift-mapping to add the lift-mapping clause :
+  %   mk-lift-mapping map MapTy (Rule map), Rule map =>
+  % Instead we choose to build a less general rule, so that we explicitly 
+  % do *not* handle non-uniform parameters for I :
+  lift-mapping a b f (FI a) (FI b) map => 
+  % The last argument.
+  @pi-decl `x` (FI a) x\
     % Build the inner match.
-    build-match I map a b f x (Match map a b f x)
+    coq.build-match 
+      x
+      (FI a)
+      (_\_\_\r\ r = FI b)
+      (build-branch I a b f)
+      (Match map a b f x)
 ]. 
-
-
-% ---------------------------------------------------------------------------------------
-
-pred build-match 
-  i:inductive, % I the inductive type.
-  i:term, % The recursive map function.
-  i:term, % A the value of the parameter we map *from*.
-  i:term, % B the value of the parameter we map *to*.
-  i:term, % f : A -> B the mapping function.
-  i:term, % x : I A the input.
-  o:term. % The resulting match statement.
-
-build-match I Map A B F X Match :-
-  coq.mk-app (global (indt I)) [A] IA,
-  coq.mk-app (global (indt I)) [B] IB,
-  coq.build-match 
-    X
-    IA
-    (_\_\_\r\ r = IB)
-    (build-branch I Map A B F)
-    Match. 
 
 % ---------------------------------------------------------------------------------------
 
 pred build-branch 
   i:inductive, % I the inductive type.
-  i:term, % The recursive map function.
   i:term, % A the value of the parameter we map *from*.
   i:term, % B the value of the parameter we map *to*.
   i:term, % f : A -> B the mapping function.
   i:term, % The branch constructor (applied to the parameters).
-  i:term, % ???
+  i:term, % The return type of the constructor.
   i:list term, % The arguments of the constructor (without the parameters).
   i:list term, % The types of each arguments.
   o:term. % The resulting branch.
 
-build-branch I Map A B F CA _ Args ArgsTy Branch :- std.do! [
+build-branch I A B F CA _ Args ArgsTy Branch :- std.do! [
   % Decide for each arg whether we leave it as is, apply F or Map to it or if it can't be handled.
   std.map2 Args ArgsTy (build-branch.aux A B F) MappedArgs, 
   % Change A with B in the constructor.
@@ -157,38 +134,28 @@ build-branch I Map A B F CA _ Args ArgsTy Branch :- std.do! [
 
 build-branch.aux A B F Arg ArgTy NewArg :-
   (copy A B => copy ArgTy NewArgTy),
-  coq.say "A =" A ", B =" B ", F =" F ", ArgTy =" ArgTy ", NewArgTy =" NewArgTy,
+  %coq.say "A =" A ", B =" B ", F =" F ", ArgTy =" ArgTy ", NewArgTy =" NewArgTy,
   lift-mapping A B F ArgTy NewArgTy FArg, 
   !, 
   coq.mk-app FArg [Arg] NewArg.
 build-branch.aux A B F Arg ArgTy NewArg :- 
   coq.error "Can't map over argument of type [" { coq.term->string ArgTy } "]".
 
-% ---------------------------------------------------------------------------------------
-
 }}.
-
 Elpi Typecheck.
 
 (**************************************************************************)
-(* Testing. *)
+(* Examples. *)
+(**************************************************************************)
 
 Inductive double A := 
   | Double : A -> A -> double A.
 Inductive tree A := 
-  | Leaf : A -> bool -> tree A
-  | Node : list (tree A) -> tree (list A) -> tree A.
-
-Inductive weird A :=
-  | Wunit : A -> weird A
-  | Wlist : list (weird (double A)) -> weird A.
+  | Leaf : A -> nat -> tree A
+  | Node : list (tree A) -> double A -> tree A -> tree A.
 
 Elpi DeriveMap double.
 Elpi AddMap (@double_map).
-Elpi AddMap (@List.map).
+Elpi DeriveMap list.
+Elpi AddMap (@list_map).
 Elpi DeriveMap tree.
-Elpi DeriveMap weird.
-
-Print double_map.
-Print tree_map.
-Print weird_map.
--- DeriveMapWIP.v
+++ DeriveMapWIP.v
@@ -4,10 +4,21 @@
 
 From elpi Require Import elpi.
 
+
+Definition prod_map (A B : Type) (f : A -> B) (A' B' : Type) (f' : A' -> B') (x : A * A') : B * B' :=
+  match x with 
+  | (a, a') => (f a, f' a')
+  end.
+
 (* We store all the mapping functions found so far in a database,
    which we can extend at will (for instance when deriving a mapping function for
    an inductive, we can add it to the database). *)
 Elpi Db map.db lp:{{
+
+% A small helper function : [copy-with As Bs T U] copies T into U while replacing each Ai with Bi.
+pred copy-with i:list term, i:list term, i:term, o:term.
+copy-with [] [] T U :- copy T U.
+copy-with [A|As] [B|Bs] T U :- copy A B => copy-with As Bs T U.
 
 % [lift-mapping As Bs FABs T U FTU] lifts the elementary mapping functions [FABi : Ai -> Bi]
 % to a more complex mapping function [FTU : T -> U].
@@ -26,40 +37,93 @@ lift-mapping As Bs FABs T U {{ fun t : lp:T => t }} :-
 
 % More rules will be added below by coq.elpi.accumulate.
 
-%lift-mapping A B FAB {{ list lp:T }} {{ list lp:U }} {{ @List.map lp:T lp:U lp:FTU }} :- 
-%  lift-mapping A B FAB T U FTU.
+% Example rule for lists.
+%lift-mapping As Bs FABs {{ list lp:T }} {{ list lp:U }} {{ @List.map lp:T lp:U lp:FTU }} :- 
+%  lift-mapping As Bs FABs T U FTU.
 
-% ----------------------------------------------------------
+% Example rule for pairs.
+%lift-mapping As Bs FABs ({{ prod lp:X1 lp:X2 }} as (T X1 X2)) ({{ prod lp:Y1 lp:Y2 }} as (U Y1 Y2)) 
+%  ({{ prod_map lp:X1 lp:Y1 lp:FXY1 lp:X2 lp:Y2 lp:FXY2 }} as (FTU X1 X2 Y1 Y2 FXY1 FXY2))
+%:-
+%  lift-mapping As Bs FABs X1 Y1 FXY1,
+%  lift-mapping As Bs FABs X2 Y2 FXY2.
+
 
 % Helper function to add a clause to lift-mapping. 
-% It takes as input a term [M] and its type of the form [forall (A B : Type), (A -> B) -> T -> U] 
-% where T does not contain B, and U is equal to T with A replaced by B. 
-% It returns a lift-mapping clause that tells how to lift a function A -> B to a function T -> U. 
+% It takes as input a term [M] and its type of the form :
+%   forall (X1 Y1 : Type), (X1 -> Y1) -> ... -> forall (Xn Yn : Type), (Xn -> Yn) -> (T -> U] 
+% where T does not contain any Yi, and U is equal to T with Xs replaced by Ys. 
+% It returns a lift-mapping clause that tells how to lift n functions Ai -> Bi to a function T -> U.
+% In full generality, this clause has the form :
+%   pi as bs fabs\
+%     pi x1 y1 fxy1\ 
+%     pi x2 y2 fxy2\ lift-mapping as bs fabs x2 y2 fxy2 =>
+%     ...
+%     pi xn yn fxyn\ 
+%     lift-mapping as bs fabs x1 y1 fxy1 =>
+% lift-mapping as bs fabs xn yn fxyn =>
+%       lift-mapping as bs fabs T U M
+% where :
+% - T contains x1, ..., xn 
+% - U contains y1, ..., yn (in fact U is equal to T with each xi replaced by yi)
+% - M is the input mapping function applied to the correct argument (i.e. x1 y1 fxy1 ... xn yn fxyn).
 % This clause can then e.g. be added to the database using coq.elpi.accumulate.
-%pred mk-lift-mapping i:term, i:term, o:prop.
-%mk-lift-mapping M 
-%  ({{ forall (A B : Type), (A -> B) -> lp:(T A B) -> lp:(U A B) }} as MTy)
-%  (pi a b fab x y fxy\ lift-mapping a b fab (T x y) (U x y) {{ lp:M lp:x lp:y lp:fxy }} :- lift-mapping a b fab x y fxy) 
-%:-
-%  std.assert-ok! (coq.typecheck M MTy) "Invalid type.",
-%  pi a b\
-%  % Check T does not contain b.
-%  not (occurs b (T a b)),
-%  % Check U is equal to T with a replaced by b.
-%  (copy a b => copy (T a b) (U a b)).
+pred mk-lift-mapping
+  i:term, % M
+  i:term, % MTy
+  o:prop. % The clause.
+mk-lift-mapping M MTy (pi as_ bs fabs\ Clause as_ bs fabs) :-
+  std.assert-ok! (coq.typecheck M MTy) "Invalid type.",
+  pi as_ bs fabs\ mk-lift-mapping.loop M MTy as_ bs fabs [] [] [] (Clause as_ bs fabs).
+
+pred mk-lift-mapping.loop 
+  i:term, % M
+  i:term, % MTy
+  i:list term, i:list term, i:list term, % as, bs, fabs
+  i:list term, i:list term, i:list term, % [xi, ..., x1] [yi, ..., y1] [fxyi, ..., fxy1]
+  o:prop. % The resulting clause.
+mk-lift-mapping.loop
+  M {{ forall (Xi Yi : Type), (Xi -> Yi) -> lp:(MTy Xi Yi) }}  
+  As Bs FABs
+  Xs Ys FXYs
+  (pi xi yi fxyi\ lift-mapping As Bs FABs xi yi fxyi => Clause xi yi fxyi)
+:-
+  !,
+  %coq.say "loop",
+  %coq.say "M=" M "MTy=" MTy,
+  %coq.say "As=" As "Bs=" Bs "FABs=" FABs,
+  %coq.say "Xs=" Xs "Ys=" Ys "FXYs=" FXYs, 
+  pi xi yi fxyi\ 
+    mk-lift-mapping.loop 
+      { coq.mk-app M [xi, yi, fxyi] } (MTy xi yi) 
+      As Bs FABs  
+      [xi|Xs] [yi|Ys] [fxyi|FXYs]
+      (Clause xi yi fxyi).
+
+% Remember that Xs, Ys and FXYs are in reversed order. This doesn't matter for us here.
+mk-lift-mapping.loop M {{ lp:T -> lp:U }} As Bs FABs Xs Ys FXYs (lift-mapping As Bs FABs T U M) :-
+  % Check T does not contain any Yi.
+  std.forall Ys (y\ not (occurs y T)),
+  % Check U is equal to T with each Xi replaced by Yi.
+  copy-with Xs Ys T U.
+
 }}. 
 Elpi Typecheck.
 
 (* Command to extend the map database. *)
-(*Elpi Command AddMap.
+Elpi Command AddMap.
 Elpi Accumulate Db map.db.
 Elpi Accumulate lp:{{
 main [trm M] :-
   std.assert-ok! (coq.typecheck M Mty) "Provided function does not typecheck",
   std.assert! (mk-lift-mapping M Mty Rule) "Provided function is not a mapping function",
+  coq.say "Added rule:" Rule,
   coq.elpi.accumulate _ "map.db" (clause _ _ Rule).
+
+main _ :- coq.error "This command expects a single term as input.".
 }}.
-Elpi Typecheck.*)
+Elpi Typecheck.
+(*Elpi AddMap (prod_map).*)
 
 Elpi Command DeriveMap.
 Elpi Accumulate Db map.db.
@@ -73,17 +137,17 @@ main [str IndName] :- std.do! [
   std.assert! (std.forall PTys (ty\ ty = sort _)) "The inductive parameters should have type [sort _] ! Aborting.",
   std.assert! (not (PTys = [])) "Inductive has no parameter! Aborting.",
   % Build the map function.
-  build-map Ind PTys [] [] [] F,
+  build-map Ind PTys F,
   coq.say { coq.term->string F },
   std.assert-ok! (coq.typecheck F _) "Resulting function does not typecheck! Aborting.",
   % Add the function to the Coq global environment.
   Name is IndName ^ "_map",
   coq.ensure-fresh-global-id Name FName,
   coq.env.add-const FName F _ @transparent! C,
-  coq.say "Added function under name" C,
+  coq.say "Added function under name" C
   % Set implicit arguments for the function.
   % For some reason we can't do this while building the function.
-  coq.arguments.set-implicit (const C) [[ maximal, maximal, explicit, explicit ]]
+  %coq.arguments.set-implicit (const C) [[ maximal, maximal, explicit, explicit ]]
 ].
 
 % ---------------------------------------------------------------------------------------
@@ -113,19 +177,45 @@ ind-params.arity (arity _) [] :- !.
 pred build-map 
   i:inductive,  % The inductive type (with parameters).
   i:list term,  % The types of the inductive parameters.
+  o:term.       % The map function we are building.
+
+% The fixpoint part. We use the argument [map] of the fixpoint to recursively map over subterms of [x] 
+% which are of type [I As]. Technically this amounts to adding a (local) clause to lift-mapping.  
+build-map I Ps (fix `map` N MapTy map\ F map) :-
+  % The index of the structurally recursive argument (starting at 0).
+  N is 3 * { std.length Ps },
+  % The type of the recursive map function.
+  build-map.ty I Ps [] [] MapTy,
+  coq.say "map type :" { coq.term->string MapTy },
+  @pi-decl `map` MapTy map\ 
+    mk-lift-mapping map MapTy (Rule map), 
+    coq.say "Recursive rule:" Rule,
+    Rule map => 
+    build-map.loop I Ps [] [] [] (F map).
+
+% Build the type of the mapping function.
+build-map.ty I [P|Ps] As Bs {{ forall (A B : lp:P), (A -> B) -> lp:(Ty A B) }} :-
+  pi a b\ build-map.ty I Ps [a|As] [b|Bs] (Ty a b).
+build-map.ty I [] As Bs {{ lp:IA -> lp:IB }} :-
+  coq.mk-app (global (indt I)) { std.rev As } IA,
+  coq.mk-app (global (indt I)) { std.rev Bs } IB.
+
+pred build-map.loop
+  i:inductive,  % The inductive type (with parameters).
+  i:list term,  % The types of the inductive parameters.
   i:list term,  % Accumulator for the types As we map from (in reversed order). 
   i:list term,  % Accumulator for the types Bs we map to (in reversed order).
   i:list term,  % Accumulator for the functions Fs : As -> Bs.
   o:term.       % The map function we are building.
 
-build-map I [P|Ps] As Bs Fs {{ fun (A B : lp:P) (f : A -> B) => lp:(Body A B f) }} :- std.do! [
+build-map.loop I [P|Ps] As Bs Fs {{ fun (A B : lp:P) (f : A -> B) => lp:(Body A B f) }} :- std.do! [
   @pi-decl `a` P a\ 
   @pi-decl `b` P b\ 
   @pi-decl `f` {{ lp:a -> lp:b }} f\
-    build-map I Ps [a|As] [b|Bs] [f|Fs] (Body a b f)
+    build-map.loop I Ps [a|As] [b|Bs] [f|Fs] (Body a b f)
 ].
 
-build-map I [] As Bs Fs (fun `x` IA x\ Match x) :- std.do! [
+build-map.loop I [] As Bs Fs (fun `x` IA x\ Match x) :- std.do! [
   std.rev As RAs,
   std.rev Bs RBs,
   std.rev Fs RFs,
@@ -136,7 +226,7 @@ build-map I [] As Bs Fs (fun `x` IA x\ Match x) :- std.do! [
       x
       IA
       (_\_\_\r\ r = IB)
-      (build-branch I _ RAs RBs RFs)
+      (build-branch I RAs RBs RFs)
       (Match x)
 ].
 
@@ -144,7 +234,6 @@ build-map I [] As Bs Fs (fun `x` IA x\ Match x) :- std.do! [
 
 pred build-branch 
   i:inductive, % I the inductive type.
-  i:term, % The recursive map function.
   i:list term, % As the types of the parameters we map *from*.
   i:list term, % Bs the types of the parameters we map *to*.
   i:list term, % fs : As -> Bs the mapping functions.
@@ -154,7 +243,7 @@ pred build-branch
   i:list term, % The types of each argument.
   o:term. % The resulting branch.
 
-build-branch I Map As Bs Fs CA _ Args ArgsTy Branch :- std.do! [
+build-branch I As Bs Fs CA _ Args ArgsTy Branch :- std.do! [
   % Decide for each arg whether we leave it as is, apply a F or Map to it or if it can't be handled.
   std.map2 Args ArgsTy (build-branch.aux As Bs Fs) MappedArgs, 
   % Change the As with the Bs in the constructor.
@@ -177,53 +266,9 @@ build-branch.aux As Bs Fs Arg ArgTy NewArg :-
 build-branch.aux A B F Arg ArgTy NewArg :- 
   coq.error "Can't map over argument of type [" { coq.term->string ArgTy } "]".
 
-% For some reason I can't write this simply as [std.forall As Bs copy => copy T U].
-pred copy-with i:list term, i:list term, i:term, o:term.
-copy-with [] [] T U :- copy T U.
-copy-with [A|As] [B|Bs] T U :- copy A B => copy-with As Bs T U.
+% ---------------------------------------------------------------------------------------
 
 }}.
-
-
-
-(*
-%(fix `map` XPos MapTy map\ (Body map)) %(A B : lp:PTy) (f : A -> B) (x : lp:GI A) {struct x} : lp:GI B := lp:(Match map A B f x) }} 
-% :- std.do! [
-%  GI = global (indt I),
-%  % The position of the structurally recursive argument [x].
-%  %XPos is 3 * { std.length PTy },
-%  % The type of the recursive mapping function.
-%  %MapTy = _, %{{ forall (A B : lp:PTy), (A -> B) -> lp:GI A -> lp:GI B }},
-%  % The fixpoint part. We use the argument [map] to recursively map over subterms of [x] 
-%  % which are of type [I A]. Technically this amounts to adding a (local) clause to lift-mapping.
-%  %@pi-decl `map` MapTy map\ %mk-lift-mapping map MapTy (Rule map), Rule map =>
-%  
-%  Res = (
-%    @funs `a` PTys as\ 
-%    @funs PTys bs\ 
-%    @funs `f` { std.map2 as bs (a\ b\ r\ r = prod _ a _\ b) } fs\ 
-%    fun `x` { coq.mk-app GI as } x\
-%      Match as bs fs x
-%  ),
-%
-%  @pi-decls `a` PTys as\
-%  @pi-decls `b` PTys bs\
-%  @pi-decls `f` { std.map2 as bs (a\ b\ r\ r = prod _ a _\ b) } fs\
-%  @pi-decl `x` { coq.mk-app GI as } x\
-%    build-match I _ as bs fs x (Match as bs fs x)
-%].
-%  
-%  % Bind the remaining arguments.
-%  @pi-decl `a` PTy a\ 
-%  @pi-decl `b` PTy b\ 
-%  @pi-decl `f` {{ lp:a -> lp:b }} f\
-%  @pi-decl `x` {{ lp:GI lp:a }} x\
-%    % Build the inner match.
-%    build-match I map a b f x (Match map a b f x)
-%]. *)
-
-
-
 
 Elpi Typecheck.
 
@@ -231,11 +276,14 @@ Elpi Typecheck.
 (* Testing. *)
 
 
-Elpi DeriveMap prod.
+Elpi DeriveMap list.
+Elpi AddMap (@list_map).
+(*Elpi DeriveMap prod.
+Elpi AddMap (@prod_map).*)
 
 Inductive multi A B :=
   | Multi1 : A -> B -> A -> multi A B
-  | Multi2 : bool -> B -> nat -> multi A B.
+  | Multi2 : bool -> list B -> nat -> multi A B.
 
 Elpi DeriveMap multi.
 
