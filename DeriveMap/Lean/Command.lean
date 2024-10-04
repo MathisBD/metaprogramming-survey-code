aa266a8c6f1abc1fff246cb8baec17ea7bb57828
MathisBD <mathis.bouverot@ens.psl.eu>
2024-10-04 13:23:23 +0200

    derivemap/Lean : added basic lifting rules

--- DeriveMap/Lean/Command.lean
+++ DeriveMap/Lean/Command.lean
@@ -1,9 +1,10 @@
 import Lean
+import Init.Control
 open Lean Meta Elab
 
 inductive Mylist (A : Type u) : Type u :=
   | nil : Bool -> Mylist A
-  | cons : A → A → Mylist A
+  | cons : A → Mylist A → Mylist A
 
 /-- `freshConstant c` returns the (universe polymorphic) constant `c` applied
     to the right number of fresh universe levels.  -/
@@ -12,16 +13,78 @@ def freshConstant (c : Name) : MetaM Expr := do
   let lvls ← cinfo.levelParams.mapM fun _ => mkFreshLevelMVar
   return .const c lvls
 
+/-- `subterm x t` is `true` if `x` occurs in `t` (modulo alpha equivalence). -/
+partial def subterm (x t : Expr) : Bool :=
+  if Expr.eqv x t
+  then true
+  else Id.run $ Expr.foldlM (fun res subt => return res || subterm x subt) false t
+
+namespace Lift
+
+/-- A lifting problem consists in lifting a function `f : A → B` to a function `T → U`.
+    It is expected that `U` is equal to `T` with `A` replaced by `B`. -/
+structure Problem where
+  A : Expr
+  B : Expr
+  f : Expr
+  T : Expr
+  U : Expr
+deriving instance Repr for Problem
+
+/-- A lifting rule tries to solve lifting problems : it either returns a solution `some f'`
+    with `f' : T → U` or fails with `none`.
+
+    Lifting rules execute in `MetaM` because they need to perform unification. -/
+def Rule := Problem → MetaM (Option Expr)
+
+/-- A basic lifting rule which solves the problem when `T` is equal to `A`,
+    in which case `f' = f`. -/
+def rule_apply : Rule := fun lp => do
+  if ← isDefEq lp.A lp.T
+  then return some lp.f
+  else return none
+
+/-- A basic lifting rule which solves the problem when `T` does not contain `A`,
+    in which case `f' = fun x : T => x`. -/
+def rule_id : Rule := fun lp => do
+  if subterm lp.A lp.T
+  then return none
+  else return some $ Expr.lam `x lp.T (.bvar 0) BinderInfo.default
+
+/-- `mzero` is a lifting rule which always fails. -/
+def mzero : Rule := fun _ => return none
+
+/-- `msum r1 r2` tries to apply rule `r1` and if it fails applies rule `r2`. -/
+def msum (r1 r2 : Rule) : Rule := fun lp => do
+  match ← r1 lp with
+  | none => r2 lp
+  | some f' => return some f'
+
+/-- `sequence rs` tries to apply the rules in `rs` in order (from left to right)
+    until one succeeds. -/
+def sequence (rs : List Rule) : Rule :=
+  match rs with
+  | [] => mzero
+  | r :: rs => List.foldl msum r rs
+
+end Lift
+
 def buildBranch (A B f : Expr) (ctr : ConstructorVal) : MetaM Expr := do
-  -- Get the type of the constructor at `A`.
+  -- Get the type of the constructor at `A`.l
   let ctr_ty ← instantiateForall ctr.type #[A]
   -- Get the arguments of the constructor.
   forallTelescope ctr_ty fun args _ => do
     -- Map over each argument of the constructor.
     let mapped_args ← args.mapM fun arg => do
+      -- Construct the lifting problem.
       let arg_ty := LocalDecl.type $ ← Meta.getFVarLocalDecl arg
-      let apply_f ← withNewMCtxDepth $ isDefEq arg_ty A
-      if apply_f then return .app f arg else return arg
+      let lp := { A, B, f, T := arg_ty, U := Expr.replaceFVar arg_ty A B : Lift.Problem }
+      -- Construct a lifting rule.
+      let rule := Lift.sequence [Lift.rule_apply, Lift.rule_id]
+      -- Solve the lifting problem.
+      match ← rule lp with
+      | none => throwError s!"Could not lift function of type {← ppExpr A} → {← ppExpr B} on argument of type {← ppExpr arg_ty}"
+      | some f' => return Expr.app f' arg
     -- Apply the constructor to the new arguments.
     let body := mkAppN (← freshConstant ctr.name) $ Array.append #[B] mapped_args
     -- Abstract with respect to the arguments.
