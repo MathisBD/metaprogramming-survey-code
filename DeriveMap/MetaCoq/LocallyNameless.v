25743bf1f1e5309c7a0b4522c35ad3d5154da2a1
MathisBD <mathis.bouverot@ens.psl.eu>
2024-10-09 10:31:49 +0200

    added comments to the locally nameless demo

--- DeriveMap/MetaCoq/LocallyNameless.v
+++ DeriveMap/MetaCoq/LocallyNameless.v
@@ -1,6 +1,7 @@
 (* Distributed under the terms of the MIT license. *)
 From MetaCoq.Utils Require Import utils.
 From MetaCoq.Template Require Import All.
+From Coq Require String.
 
 Import MCMonadNotation.
 
@@ -113,23 +114,43 @@ Fixpoint find_map {A B} (pred : A -> option B) (l : list A) : option B :=
 End List.
 
 (*******************************************************************************)
-(** This module defines named contexts, i.e. local contexts which store
-    named assumptions or definitions. *)
+(** This module defines named contexts which store variable declarations 
+    indexed by identifiers [ident]. We could use virtually anything in place of identifiers,
+    but this choice makes it easy to embed a variable in a term using [tVar]. 
+    
+    Introducing a declaration in the context shadows previous declarations with the same identifier.
+*)
 (*******************************************************************************)
 Module NamedContext.
 
-Record t := mk { decls : list (ident * context_decl) }.
+Record t := mk 
+  { (* We store the [ident * declaration] pairs in order, the most recent first. 
+       A more efficient implementation would perhaps use a map. *)
+    decls : list (ident * context_decl)
+  ; (* The seed is used to generate fresh identifiers. *)
+    seed : nat }.
 
-Definition empty : t := {| decls := [] |}.
+(** The empty named context. *)
+Definition empty : t := {| decls := [] ; seed := 0 |}.
 
+(** Add a declaration to the context, shadowing previous declarations with the same identifier. *)
 Definition push (id : ident) (decl : context_decl) (ctx : t) : t := 
-  {| decls := (id, decl) :: ctx.(decls) |}.
+  {| decls := (id, decl) :: ctx.(decls) ; seed := ctx.(seed) |}.
 
+(** [fresh_ident ctx basename] builds a fresh identifier from [basename].
+    It should be distinct from any other identifier constructed this way using [ctx]. *)
+Definition fresh_ident (ctx : t) (basename : string) : t * ident :=
+  let id := (string_of_nat ctx.(seed) ++ "#" ++ basename)%bs in
+  let ctx := {| decls := ctx.(decls) ; seed := ctx.(seed) + 1 |} in
+  (ctx, id).
+
+(** [get_decl id ctx] retrieves the most recent declaration for [id] in [ctx]. *)
 Definition get_decl (id : ident) (ctx : t) : option context_decl :=
   List.find_map 
     (fun '(id', decl) => if id == id' then Some decl else None) 
     ctx.(decls).
 
+(** Same as [get_decl] but returns only the type. *)
 Definition get_type (id : ident) (ctx : t) : option term :=
   option_map decl_type (get_decl id ctx).
 
@@ -138,6 +159,7 @@ End NamedContext.
 (*******************************************************************************)
 (** A bare bones locally nameless API. *)
 (*******************************************************************************)
+
 Fixpoint abstract_aux (id : ident) (depth : nat) (t : term) : term :=
   match t with 
   | tVar id' => if id == id' then tRel depth else t
@@ -155,7 +177,8 @@ Definition abstract (id : ident) (t : term) : term := abstract_aux id 0 t.
     - the identifier of the added local variable. *)
 Definition with_local_decl {T} (decl : context_decl) (ctx : NamedContext.t) (k : NamedContext.t -> ident -> T) : T :=
   (* Create a fresh identifier. *)
-  let id := "TODO" in 
+  let basename := string_of_name decl.(decl_name).(binder_name) in
+  let (ctx, id) := NamedContext.fresh_ident ctx basename in 
   (* Pass the identifier and extended context to the continuation. *)
   k (NamedContext.push id decl ctx) id.
 
@@ -163,9 +186,9 @@ Definition with_local_decl {T} (decl : context_decl) (ctx : NamedContext.t) (k :
     The body has access to an identifier and to an extended named context containing a declaration for this identifier.
 
     For instance here is how to create the identity function at type [T] :
-      [mk_lambda xname T ctx $ fun ctx x => tVar x]
+      [mk_lambda name T ctx $ fun ctx x => tVar x]
 *)
 Definition mk_lambda (name : aname) (ty : term) (ctx : NamedContext.t) (mk_body : NamedContext.t -> ident -> term) : term :=
-  withLocalDecl {| decl_name := name ; decl_body := None ; decl_type := ty |} ctx $ fun ctx id => 
+  with_local_decl {| decl_name := name ; decl_body := None ; decl_type := ty |} ctx $ fun ctx id => 
     let body := mk_body ctx id in
     tLambda name ty $ abstract id body.
